822d740afee59b7b4a15dde2879e24ee5466791c
Aaron Blakely <aaron@ephasic.org>
2012-08-04 14:04:48 -0500

    bug fixes... Should work properly... Gotta stop commiting untested code.

--- auto-runner.sh
+++ auto-runner.sh
@@ -4,11 +4,11 @@
 # This code is public domain.  You can do whatever
 # you want with it.
 
-RUNDIR=".wp-updater/"	# Make sure this matches with wp-updater.sh
+RUNDIR="$HOME/.wp-updater"	# Make sure this matches with wp-updater.sh
 
 if [ -a /tmp/wp-updater.lock ]; then
 	echo "WP updater already running..." >> /dev/null	# he he
 else
-	. $RUNDIR/wp-updater.sh
+	. $RUNDIR/wp-updater.sh &
 	touch /tmp/wp-updater.lock
 fi
--- wp-updater.sh
+++ wp-updater.sh
@@ -4,27 +4,23 @@
 # This code is public domain.  You can do whatever
 # you want with it.
 
-RUNDIR=".wp-updater/"	# Directory which this script is ran from
-MONITOR_COUNT=1		# The count of monitors, starting at 0.
-IMGDIR="~/Pictures/wp"	# Directory where the images are.
-INTTIME="15m"		# sleep(1) paramater
+RUNDIR="$HOME/.wp-updater/"		# Directory which this script is ran from
+MONITOR_COUNT=2				# The count of monitors.
+IMGDIR="$HOME/Pictures/wallpapers"	# Directory where the images are.
+INTTIME="15m"				# sleep(1) paramater
 
 cd "$RUNDIR"
-
-
 while [ 1 ]; do
-	for ((i=0; i < MONITOR_COUNT; i++)); do
-		echo `ls -d -1 ${IMGDIR}/**` > listu.tmp	# Get the image list
+	for ((i=0; i < $MONITOR_COUNT; i++)); do
+		echo `ls -d -1 ${IMGDIR}/**` > /tmp/listu.tmp	# Get the image list
 
 		# As far as I can tell, this next line is required...  Or I was doing something wrong before.
-		echo "# xfce backdrop list" > list.tmp
-		tr ' ' '\n' < listu.tmp >> list.tmp		# Replace spaces with newlines
-		IMG_PATH="`pwd`/list"
+		echo "# xfce backdrop list" > /tmp/list.tmp
+		tr ' ' '\n' < /tmp/listu.tmp >> /tmp/list.tmp		# Replace spaces with newlines
+		IMG_PATH="/tmp/list.tmp"
 
 		xfconf-query -c xfce4-desktop -p "/backdrop/screen0/monitor${i}/image-path" -s ""		# Clear the current list
-		xfconf-query -c xfce4-desktop -p "/backdrop/screen0/monitor${i}/image-path" -s "${IMAGE_PATH}"	# Load the new one
-
-		rm listu.tmp list.tmp	# cleanup the tmp files
+		xfconf-query -c xfce4-desktop -p "/backdrop/screen0/monitor${i}/image-path" -s "${IMG_PATH}"	# Load the new one
 	done
 
 	sleep $INTTIME
